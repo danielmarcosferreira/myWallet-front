f87311705c46d649444c593dd3e739dd0edd456e
Daniel <daniel.marcos.1038@gmail.com>
2024-05-15 12:44:38 -0300

    feat: Not Found Page

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import App from './App.jsx'
+import SignInPage from './pages/SignInPage/SignInPage.jsx'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignInPage />,
+    errorElement: <NotFoundPage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ src/pages/SignInPage/SignInPage.jsx
@@ -0,0 +1,7 @@
+export default function SignInPage() {
+    return (
+        <>
+            Sign In Page
+        </>
+    )
+}
